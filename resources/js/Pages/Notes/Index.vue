30a8a97315cf857da6a38398cc37ddeada48c566
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-06-03 08:25:59 -0600

    listing notes: personalization

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -1,7 +1,3 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-</script>
-
 <template>
     <AppLayout title="Dashboard">
         <template #header>
@@ -17,9 +13,29 @@ import AppLayout from '@/Layouts/AppLayout.vue';
                         <div class="px-4 sm:px-0">
                             <h3 class="text-lg text-white">Listado de notas</h3>
                             <p class="text-sm text-white">Toma el registro correcto y ejecuta cualquier función
-                                (ver,
-                                editar
-                                o eliminar)</p>
+                                (ver, editar o eliminar)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-black md:rounded-md p-4 text-white">
+                            <table>
+                                <tr v-for="(note, i) in notes" :key="i">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="border px-4 py-2">
+                                        <NavLink :href="route('notes.show', note.id)">
+                                            Ver
+                                        </NavLink>
+                                    </td>
+                                    <td class="border px-4 py-2">
+                                        <NavLink :href="route('notes.edit', note.id)">
+                                            Editar
+                                        </NavLink>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -27,3 +43,12 @@ import AppLayout from '@/Layouts/AppLayout.vue';
         </div>
     </AppLayout>
 </template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import NavLink from '@/Components/NavLink.vue';
+
+const props = defineProps({
+    notes: Array,
+});
+</script>
