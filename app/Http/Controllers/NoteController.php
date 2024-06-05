fc76d3617a6b055db6660bd1cb8264f2e300138f
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-06-05 09:00:23 -0600

    flash message

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -34,7 +34,7 @@ class NoteController extends Controller
             'content' => 'required',
         ]);
         $note = Note::create($request->all());
-        return redirect()->route('notes.edit', $note->id);
+        return redirect()->route('notes.edit', $note->id)->with('status', 'Nota creada');
     }
 
     /**
@@ -63,7 +63,7 @@ class NoteController extends Controller
             'content' => 'required',
         ]);
         $note->update($request->all());
-        return redirect()->route('notes.index');
+        return redirect()->route('notes.index')->with('status', 'Nota actualizada');
     }
 
     /**
@@ -72,6 +72,6 @@ class NoteController extends Controller
     public function destroy(Note $note)
     {
         $note->delete();
-        return redirect()->route('notes.index');
+        return redirect()->route('notes.index')->with('status', 'Nota eliminada');
     }
 }
