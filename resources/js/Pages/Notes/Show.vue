0c0fee844e2b52ce82ed9ce3fdc7157bac33e1af
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-06-05 10:53:27 -0600

    ux details to show

--- resources/js/Pages/Notes/Show.vue
+++ resources/js/Pages/Notes/Show.vue
@@ -10,17 +10,18 @@
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="md:col-span-1">
                         <div class="px-4 sm:px-0">
-                            <h3 class="text-lg text-white">Detalle de una nota</h3>
-                            <p class="text-sm text-white mt-2">
+                            <h3 class="text-lg text-gray-200">Extracto de la nota</h3>
+                            <p class="text-sm text-gray-400 mt-2">
                                 {{ note.excerpt }}
                             </p>
                         </div>
                     </div>
-                    <div class="md:col-span-2 mt-5 md:mt-0">
-                        <div class="shadow bg-black md:rounded-md p-4 text-white">
+                    <div class="bg-gray-800 rounded-md md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow md:rounded-md p-4 text-gray-300">
+                            <h2 class="my-2 text-white text-lg font-bold">Contenido:</h2>
                             <p>{{ note.content }}</p>
                             <hr class="mt-6 mb-6">
-                            <Link :href="route('notes.index')">Volver</Link>
+                            <Link class="text-blue-400" :href="route('notes.index')">Volver</Link>
                         </div>
                     </div>
                 </div>
@@ -31,7 +32,6 @@
 
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import NavLink from '@/Components/NavLink.vue';
 import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
